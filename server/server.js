8fa939a64de03073e8eddc723ff5a6011b3ffcaf
santhoshsaravanamuthu <santhoshs.csbs2020@citchennai.net>
2023-02-24 14:07:31 +0530

    first code

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 import Connection from './database/db.js';
 import Router from './routes/route.js';
 
@@ -10,14 +9,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use('/', Router);
-
-if (process.env.NODE_ENV === 'production'){     
-    app.use(express.static('client/bulid'));
-}
-
 
 const PORT = process.env.PORT || 8000;
 
